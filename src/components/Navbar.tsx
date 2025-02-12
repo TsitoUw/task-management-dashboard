557aecce8e9f27e00f5e1f4f8e98df06eb818066
TsitoUw <tsitohaina.pro@gmail.com>
2025-02-12 22:31:39 +0300

    [update] home page title

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ function Navbar() {
         <nav className="w-full bg-background sticky top-0 shadow mb-4">
             <ul className="container mx-auto p-4">
                 <li>
-                    <p className="font-bold text-2xl">Dashboard</p>
+                    <h1 className="font-bold text-2xl">Task Dashboard</h1>
                 </li>
             </ul>
         </nav>
