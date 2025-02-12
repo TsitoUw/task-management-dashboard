38f1f3c2b67aed352b8ccd9fe9c81f55b2295cb3
TsitoUw <tsitohaina.pro@gmail.com>
2025-02-12 21:40:50 +0300

    [update] navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 function Navbar() {
     return (
-        <nav className="container mx-auto p-2 bg-background sticky top-0">
-            <ul>
+        <nav className="w-full bg-background sticky top-0 shadow mb-4">
+            <ul className="container mx-auto p-2 py-4">
                 <li>
                     <p className="font-bold text-2xl">Dashboard</p>
                 </li>
